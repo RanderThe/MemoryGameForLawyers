afd3918a52db522d4df36f02c995acfd12c6b413
RanderThe <randerlealt@gmail.com>
2020-05-07 02:30:52 -0300

    Ajuste Splash e Estrutura do APP
    Ajuste Splash e Estrutura do APP

--- MemoryGameForLawyers/MemoryGameForLawyers.Android/MainActivity.cs
+++ MemoryGameForLawyers/MemoryGameForLawyers.Android/MainActivity.cs
@@ -17,7 +17,7 @@ namespace MemoryGameForLawyers.Droid
   [Activity(Label = "Escolha Direito", 
     Icon = "@drawable/EscolhaDireito", 
     Theme = "@style/MainTheme", 
-    MainLauncher = true,
+    MainLauncher =false,
     ScreenOrientation = ScreenOrientation.Portrait,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 
@@ -33,15 +33,6 @@ namespace MemoryGameForLawyers.Droid
       PullToRefreshLayoutRenderer.Init();
       Popup.Init(this, savedInstanceState);
       Plugin.InputKit.Platforms.Droid.Config.Init(this, savedInstanceState);
-      if (Intent.Extras != null)
-      {
-        foreach (var key in Intent.Extras.KeySet())
-        {
-          var value = Intent.Extras.GetString(key);
-          Log.Debug("MainActivity", "Key: {0} Value: {1}", key, value);
-        }
-      }
-
       Xamarin.Essentials.Platform.Init(this, savedInstanceState);
       global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
       LoadApplication(new App());
